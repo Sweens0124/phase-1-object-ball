8fded1df9bbb684db1d5eb13b45fd4da78b08771
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-19 16:09:15 -0600

    solved numPointsScored function

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -59,61 +59,82 @@ function gameObject () {
       }
     },
     away: {
-        "teamName": "Charlotte Hornets",
-        "colors": ['Turquoise', 'Purple'],
-        "players": {
-          "Jeff Adrien": {
-            "number": 4,
-            "shoe": 18,
-            "points": 10,
-            "rebounds": 1,
-            "assists": 1, 
-            "steals": 2,
-            "blocks": 7,
-            "slamDunks": 2 
-          },
-          "Bismak Biyombo": {
-            "number": 0,
-            "shoe":  16,
-            "points": 12,
-            "rebounds": 4,
-            "assists":  7,  
-            "steals":  7,
-            "blocks":  15,
-            "slamDunks": 10
-          },
-          "DeSagna Diop": {
-            "number": 2,
-            "shoe": 14,
-            "points": 24,
-            "rebounds": 12,
-            "assists": 12,  
-            "steals": 4,
-            "blocks": 5,
-            "slamDunks": 5 
-          },
-          "Ben Gordon": {
-            "number": 8,
-            "shoe": 15,
-            "points": 33,
-            "rebounds": 3,
-            "assists": 2, 
-            "steals": 1,
-            "blocks": 1,
-            "slamDunks": 0 
-          },
-          "Brendan Haywood": {
-            "number": 33,
-            "shoe":  15,
-            "points": 6,
-            "rebounds": 12,
-            "assists": 12, 
-            "steals":  22,
-            "blocks": 5,
-            "slamDunks": 12 
-          }
+      "teamName": "Charlotte Hornets",
+      "colors": [ 'Turquoise', 'Purple' ],
+      "players": {
+        "Jeff Adrien": {
+          "number": 4,
+          "shoe": 18,
+          "points": 10,
+          "rebounds": 1,
+          "assists": 1,
+          "steals": 2,
+          "blocks": 7,
+          "slamDunks": 2
+        },
+        "Bismak Biyombo": {
+          "number": 0,
+          "shoe": 16,
+          "points": 12,
+          "rebounds": 4,
+          "assists": 7,
+          "steals": 7,
+          "blocks": 15,
+          "slamDunks": 10
+        },
+        "DeSagna Diop": {
+          "number": 2,
+          "shoe": 14,
+          "points": 24,
+          "rebounds": 12,
+          "assists": 12,
+          "steals": 4,
+          "blocks": 5,
+          "slamDunks": 5
+        },
+        "Ben Gordon": {
+          "number": 8,
+          "shoe": 15,
+          "points": 33,
+          "rebounds": 3,
+          "assists": 2,
+          "steals": 1,
+          "blocks": 1,
+          "slamDunks": 0
+        },
+        "Brendan Haywood": {
+          "number": 33,
+          "shoe": 15,
+          "points": 6,
+          "rebounds": 12,
+          "assists": 12,
+          "steals": 22,
+          "blocks": 5,
+          "slamDunks": 12
         }
+      }
     }
   }
   return gameObjects
 }
+
+function numPointsScored (playersName) {
+  const gameObjects1 = gameObject()
+  const home = gameObjects1.home.players
+  const away = gameObjects1.away.players
+  for (player in home) {
+    // console.log(player[ points ])
+    console.log(home[ 'Alan Anderson' ][ 'points' ])
+    if (player === playersName) {
+
+      return playersName[ points ]
+    }
+  }
+  for (player in away) {
+    if (player === playersName) {
+      return playersName[ points ]
+    }
+  }
+}
+
+console.log(numPointsScored([ 'Jason Terry' ]))
