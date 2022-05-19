4dec6543aa284dbc082c14f1115afb7028c36eb3
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-19 15:25:10 -0600

    updated first 'home' key

---
+++ src/00-objectball.js
@@ -0,0 +1,65 @@
+// console.log('hello')
+
+function gameObject () {
+  const gameObjects = {
+    home: {
+      teamName: 'Brooklyn Nets',
+      colors: [ 'black', 'white' ],
+      players: {
+        'Alan Anderson': {
+          'numbers': 0,
+          'shoe': 16,
+          'points': 22,
+          'rebounds': 12,
+          'assists': 12,
+          'steals': 3,
+          'blocks': 1,
+          'slamDunks': 1
+        },
+        'Alan Anderson': {
+          'numbers': 30,
+          'shoe': 14,
+          'points': 12,
+          'rebounds': 12,
+          'assists': 12,
+          'steals': 12,
+          'blocks': 12,
+          'slamDunks': 7
+        },
+        'Alan Anderson': {
+          'numbers': 11,
+          'shoe': 17,
+          'points': 17,
+          'rebounds': 19,
+          'assists': 10,
+          'steals': 3,
+          'blocks': 1,
+          'slamDunks': 15
+        },
+        'Alan Anderson': {
+          'numbers': 1,
+          'shoe': 19,
+          'points': 26,
+          'rebounds': 12,
+          'assists': 6,
+          'steals': 3,
+          'blocks': 8,
+          'slamDunks': 5
+        },
+        'Alan Anderson': {
+          'numbers': 31,
+          'shoe': 15,
+          'points': 19,
+          'rebounds': 2,
+          'assists': 2,
+          'steals': 4,
+          'blocks': 11,
+          'slamDunks': 1
+        },
+      }
+    },
+    away: {
+
+    }
+  }
+}
